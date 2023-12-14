7f5064a310965acad31a121dd1cbd6faae11a57c
Colin Heurteaux <colin.heurteaux@epitech.eu>
2023-12-14 14:23:50 +0100

    Added error handling for sort add, and del + restructured code for sort function

--- internal_functions/data_manipulation.c
+++ internal_functions/data_manipulation.c
@@ -6,7 +6,6 @@
 */
 
 #include <stdbool.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "../lib/my.h"
@@ -16,26 +15,34 @@
 #include "data_check.h"
 #include "data_info.h"
 
+static void setup_new_node(linked_list_node_t *new_node,
+    linked_list_node_t **head, int i, char **args)
+{
+    if (*head == NULL) {
+        new_node->id = 0;
+        new_node->next = NULL;
+    } else {
+        new_node->id = (*head)->id + 1;
+        new_node->next = *head;
+    }
+    new_node->type = my_strdup(args[i]);
+    new_node->name = my_strdup(args[i + 1]);
+    *head = new_node;
+    display_on_add(new_node);
+}
+
 int add(void *data, char **args)
 {
     linked_list_node_t *new_node;
     linked_list_node_t **head = (linked_list_node_t **) data;
 
+    if (get_args_len(args) < 1)
+        return 84;
     for (int i = 0; i < get_args_len(args); i += 2) {
         new_node = malloc(sizeof(linked_list_node_t));
         if (check_type(args[i]) == 1 || args[i + 1] == NULL)
             return 84;
-        if (*head == NULL) {
-            new_node->id = 0;
-            new_node->next = NULL;
-        } else {
-            new_node->id = (*head)->id + 1;
-            new_node->next = *head;
-        }
-        new_node->type = my_strdup(args[i]);
-        new_node->name = my_strdup(args[i + 1]);
-        *head = new_node;
-        display_on_add(new_node);
+        setup_new_node(new_node, head, i, args);
     }
     return 0;
 }
@@ -87,6 +94,8 @@ int del(void *data, char **args)
 {
     linked_list_node_t **head = (linked_list_node_t **) data;
 
+    if (get_args_len(args) < 1)
+        return 84;
     for (int i = 0; args[i] != NULL; i++) {
         if (*head == NULL) {
             return 84;
@@ -98,20 +107,37 @@ int del(void *data, char **args)
     return 0;
 }
 
+static bool is_valid_arg(char *arg, void *data, bool is_reversed)
+{
+    if (my_strcmp(arg, "TYPE") == 0) {
+        sort_by_type(data, is_reversed);
+        return true;
+    }
+    if (my_strcmp(arg, "NAME") == 0) {
+        sort_by_name(data, is_reversed);
+        return true;
+    }
+    if (my_strcmp(arg, "ID") == 0) {
+        sort_by_id(data, is_reversed);
+        return true;
+    }
+    if (my_strcmp(arg, "-r") == 0) {
+        return true;
+    }
+    return false;
+}
+
 int sort(void *data, char **args)
 {
     bool is_reversed = false;
 
+    if (*(linked_list_node_t **)data == NULL)
+        return 0;
     for (int i = get_args_len(args) - 1; i >= 0; i--) {
         if (args[i + 1] != NULL)
             is_reversed = (my_strcmp(args[i + 1], "-r") == 0);
-        if (my_strcmp(args[i], "TYPE") == 0)
-            sort_by_type(data, is_reversed);
-        if (my_strcmp(args[i], "NAME") == 0)
-            sort_by_name(data, is_reversed);
-        if (my_strcmp(args[i], "ID") == 0)
-            sort_by_id(data, is_reversed);
-        if (my_strcmp(args[i], "-r") == 0)
+        if (is_valid_arg(args[i], data, is_reversed))
+            continue;
         return 84;
     }
     return 0;
